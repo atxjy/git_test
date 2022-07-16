59bc9798cb33e88d4251620be6666253751c4c21
xjy <3360907452@qq.com>
2022-07-16 08:46:01 +0800

    first commit

---
+++ src/main/java/com/atxjy/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atxjy.git;
+
+/**
+ * @author xjy
+ * @create 2022-07-16 8:40
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        System.out.println("hello git!");
+
+    }
+}
